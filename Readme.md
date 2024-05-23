caad534a475f7ec1a8626af8758143f380e084a0
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2024-05-24 00:07:22 +0600

    Readme.md added

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Used Scripts in this project:
+
+
+* [Tailwind CSS](https://tailwindcss.com)
+* [Daisy UI](https://daisyui.com/)
+
+* [FontAwesome](https://fontawesome.com)
--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.querySelector('.card').addEventListener('click', function(e) {
-  document.querySelector('.card').id = 'hle';
+// Add item to the cart
+function addToCart(name, price) {
+  console.log(document.getElementById(id));
+}
+
+// Added id to the clicked card and send to Cart
+document.querySelector('.card').addEventListener('click', function() {
+  document.querySelector('.product-name').id = 'product-name';
+  document.querySelector('.product-price').id = 'product-price';
   console.log(document.querySelector('.card'));
 })
