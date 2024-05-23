868bf96a908a0bcb6d43ddb18aded5d597cf49b9
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2024-05-23 14:43:28 +0600

    after a long research and logic, i can now add id to an clicked element

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.querySelector('.card').addEventListener('click', function(e) {
-  console.log(document.querySelector('.card').classList);
-  document.querySelector('.card').classList.add('hle');
-  console.log(document.querySelector('.card').classList);
+  document.querySelector('.card').id = 'hle';
+  console.log(document.querySelector('.card'));
 })
